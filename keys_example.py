008c795c0055eb37123db2e9c8d4f490428e8222
dsymbol <88138099+dsymbol@users.noreply.github.com>
2021-09-29 17:51:39 +0300

    Initial commit

---
+++ keys_example.py
@@ -0,0 +1,6 @@
+#  Set the fields accordingly and rename to keys.py
+
+discord_channel = 123456789123456789
+discord_token = "gsHQ5wpRUHUiS8cW1lq55iyr.H3XifBCbs8dmUxaYinen8rQcPyLDJgKoZK"
+telegram_token = "1234567891:NrKveoWNZjDXR5p99nUk_g8wmrNU3GsTmrS"
+telegram_chatid = -1234567891234
---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from telegram import Bot
+from keys import *  
+
+dc = discord.Client()
+tg = Bot(token=telegram_token)
+    
+@dc.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(dc))
+
+@dc.event
+async def on_message(message): 
+    finalMessage = message.content
+    if (message.channel.id == discord_channel):
+        print("Forwarding: " + finalMessage)
+        tg.sendMessage(telegram_chatid, finalMessage)
+
+dc.run(discord_token)
