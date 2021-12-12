a061f22ed6e9fba86a3513bccacf2db5519d16f5
dsymbol <dsymbol@users.noreply.github.com>
2021-12-12 21:32:49 +0200

    Added attachment support

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ tg = Bot(token=TELEGRAM_TOKEN)
 @dc.event
 async def on_message(message): 
     if message.channel.id in DISCORD_CHANNELS:
-        print(f"[+] {message.content}")
-        tg.sendMessage(TELEGRAM_CHAT_ID, message.content)
+        if message.attachments:
+            attachers = ""
+            for attachment in message.attachments:
+                attachers += f"{attachment} "
+            print(f'{message.content} {attachers}')
+            tg.sendMessage(TELEGRAM_CHAT_ID, f'{message.content} {attachers}')
+        else:
+            print(f"{message.content}")
+            tg.sendMessage(TELEGRAM_CHAT_ID, message.content)
 
 def main():
-    print("[!] Bot running, waiting for messages...")
+    print("Running, waiting for messages...")
     dc.run(DISCORD_TOKEN)
 
 if __name__ == '__main__':
